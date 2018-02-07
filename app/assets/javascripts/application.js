ff97de74330cd4bfe5b21accfb499ecdfe515cb9
cbryant24 <chrs.bryant@gmail.com>
2018-02-06 23:49:51 -0800

    added footer and background image

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery_ujs
 //= require jquery
-//= require bootstrap-sprockets
 //= require rails-ujs
+//= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
