3358e51fc3eafcf3fcaa425800d176bc4d686738
cbryant24 <chrs.bryant@gmail.com>
2018-02-11 18:12:56 -0800

    signup users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
 
   root 'pages#home'
   get '/about', to: 'pages#about'
-
+  get '/signup', to: 'users#new'
   resources :articles
+
+  post '/users', to: 'users#create'
+  resources :users, except: [:new]
 end
